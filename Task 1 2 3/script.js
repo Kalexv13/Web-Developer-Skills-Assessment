e16092cf7230daab92a639e7d651736f06011d72
Kalex Vargas <55945532+Kalexv13@users.noreply.github.com>
2024-09-09 00:40:46 -0300

    Add files via upload

---
+++ Task 1 2 3/script.js
@@ -0,0 +1,11 @@
+function performCalculation() {
+
+    var num1 = 2;
+    var num2 = 2;
+    
+  
+    var result = num1 + num2;
+    
+  
+    document.getElementById('result').textContent = 'The result of the addition is: ' + result;
+}
